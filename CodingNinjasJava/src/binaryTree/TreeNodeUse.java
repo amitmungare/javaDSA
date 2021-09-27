8e03bc775a98559d7978e282ffb6b60aa37c8c33
Amit Mungare <amungare27@gmail.com>
2021-09-27 22:10:03 +0530

    tree node use

---
+++ CodingNinjasJava/src/binaryTree/TreeNodeUse.java
@@ -0,0 +1,98 @@
+package binaryTree;
+
+import java.util.Scanner;
+
+import queue.QueueLL;
+
+
+
+public class TreeNodeUse {
+	
+	public static void printR(TreeNode<Integer> root) {
+		if(root==null)return;
+		
+		System.out.println(root.data);
+		
+		printR(root.left);
+		printR(root.right);
+		
+	}
+	
+	public static TreeNode<Integer> inputR() {
+		
+		Scanner sc = new Scanner(System.in);
+		int rootData = sc.nextInt();
+		
+		if(rootData == -1)return null;
+		
+		TreeNode<Integer> root = new TreeNode<>(rootData);
+		
+		TreeNode<Integer> rootleft = inputR();
+		TreeNode<Integer> rootright = inputR();
+		root.left = rootleft;
+		root.right = rootright;
+		
+		return root;
+	}
+
+	public static TreeNode<Integer> inputLW(){
+		Scanner sc = new Scanner(System.in);
+		QueueLL<TreeNode<Integer>> pendingNode = new QueueLL<>();
+		
+		int rootdata = sc.nextInt();
+		if(rootdata == -1)return null;
+		
+		TreeNode<Integer> root = new TreeNode<Integer>(rootdata);
+		pendingNode.enqueue(root);
+		
+		while(!pendingNode.isEmpty()) {
+			TreeNode<Integer> front = pendingNode.dequeue();
+			
+			int leftchild = sc.nextInt();
+			if(leftchild != -1) {
+				TreeNode<Integer> child = new TreeNode<Integer>(leftchild);
+				pendingNode.enqueue(child);
+				front.left = child;
+			}
+			
+			int rightchild = sc.nextInt();
+			if(rightchild != -1) {
+				TreeNode<Integer> child = new TreeNode<Integer>(rightchild);
+				pendingNode.enqueue(child);
+				front.right = child;
+			}		
+		}
+		return root;
+	}
+	
+	public static void printLW(TreeNode<Integer> root) {
+		
+		QueueLL<TreeNode<Integer>> q = new QueueLL<>();
+		q.enqueue(root);
+		q.enqueue(null);
+		
+		while(!q.isEmpty()) {
+			TreeNode<Integer> front  =q.dequeue();
+			if(front == null) {
+				if(q.isEmpty())break;
+				System.out.println();
+				q.enqueue(null);
+			}else {
+				System.out.print(front.data+" ");
+				if(front.left != null)q.enqueue(front.left);
+				if(front.right != null)q.enqueue(front.right);
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+//		printR(inputR());
+		
+		printLW(inputLW());
+		
+	}
+
+}
