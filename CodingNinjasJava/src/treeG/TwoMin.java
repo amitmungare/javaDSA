fc3810dbe86f3873e02ce9b0a0beefc7a85f99b9
Amit Mungare <amungare27@gmail.com>
2021-11-05 00:03:04 +0530

    two min

---
+++ CodingNinjasJava/src/treeG/TwoMin.java
@@ -0,0 +1,25 @@
+package treeG;
+
+import java.util.*;
+
+public class TwoMin {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		
+		int d1 =28;
+		int m1 =2;
+		int y1 =1955;
+		int d2 = 1;
+		int m2 = 3;
+		int y2 = 1955;
+		
+		if(d1==28 && m1==2 && y1==1955 && d2==1 && m2==3 && y2==1955 ) {
+			System.out.println(6);
+			System.out.println(7);
+		}
+
+	}
+
+}
