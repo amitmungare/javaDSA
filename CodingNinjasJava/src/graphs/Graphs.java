2bb96319b845ada94bb68355580bdfb1c9d89159
Amit Mungare <amungare27@gmail.com>
2021-11-09 23:47:15 +0530

    graphs

--- CodingNinjasJava/src/graphs/Graphs.java
+++ CodingNinjasJava/src/graphs/Graphs.java
@@ -8,7 +8,14 @@ public class Graphs {
 	
 	public static void dfs(int mat[][]) {
 		boolean v[] = new boolean[mat.length];
-		dfs(mat,0, v);
+		
+		for(int i=0; i<mat.length; i++) {
+			if(!v[i]) {
+				dfs(mat,i, v);
+			}
+		}
+		
+		
 	}
 	
 	public static void dfs(int mat[][], int curr, boolean v[]) {
@@ -53,8 +60,8 @@ public class Graphs {
 			mat[v1][v2]=1;
 			mat[v2][v1]=1;
 		}
-//		dfs(mat);
-		bfs(mat);
+		dfs(mat);
+//		bfs(mat);
 	}
 
 }
