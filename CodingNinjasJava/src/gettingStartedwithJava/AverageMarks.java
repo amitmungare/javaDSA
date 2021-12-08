1adfe9fe84c973672062ea725bef86d5c88af8cc
Amit Mungare <amungare27@gmail.com>
2021-12-08 21:19:36 +0530

    averagemarks

---
+++ CodingNinjasJava/src/gettingStartedwithJava/AverageMarks.java
@@ -0,0 +1,20 @@
+package gettingStartedwithJava;
+import java.util.*;
+
+public class AverageMarks {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		char a =sc.next().charAt(0);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=sc.nextInt();
+		
+		System.out.println(a);
+		System.out.println((x+y+z)/3);
+		
+	}
+
+}
